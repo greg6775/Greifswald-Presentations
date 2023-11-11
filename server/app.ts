67f748b9e5f256b7c4a8e67d2dc2a71c94f86fca
Skidder <Skidder@bbn.one>
2023-11-11 23:10:10 +0100

    add limit

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,6 @@
 import { serveDir } from "std/http/file_server.ts";
 import { MongoClient } from "mongo/mod.ts";
+import zod from "https://deno.land/x/zod@v3.22.2/index.ts";
 
 const client = new MongoClient();
 
@@ -11,7 +12,24 @@ await Deno.serve({ port: 8080 }, async (req) => {
     const url = new URL(req.url)
     if (url.pathname == "/submit" && req.method == "POST") {
         const body = new TextDecoder().decode(await req.arrayBuffer());
-        const data = JSON.parse(body);
+        const data = zod.object({
+            name: zod.string({ invalid_type_error: "Fehlender Name" }),
+            p1: zod.string({ invalid_type_error: "Vortrag 1 fehlt" }),
+            p2: zod.string({ invalid_type_error: "Vortrag 2 fehlt" }),
+            p3: zod.string({ invalid_type_error: "Vortrag 3 fehlt" }),
+            p4: zod.string({ invalid_type_error: "Vortrag 4 fehlt" }),
+        }).parse(JSON.parse(body));
+
+        //count submissions
+        if ((await db.collection("submissions").find({ p1: data.p1 }).toArray()).length > 20)
+            return new Response("Vortrag 1 ist schon voll", { status: 400 });
+        if ((await db.collection("submissions").find({ p2: data.p2 }).toArray()).length > 20)
+            return new Response("Vortrag 2 ist schon voll", { status: 400 });
+        if ((await db.collection("submissions").find({ p3: data.p3 }).toArray()).length > 20)
+            return new Response("Vortrag 3 ist schon voll", { status: 400 });
+        if ((await db.collection("submissions").find({ p4: data.p4 }).toArray()).length > 20)
+            return new Response("Vortrag 4 ist schon voll", { status: 400 });
+
         const collection = db.collection("submissions");
         await collection.insertOne(data);
         return new Response()
--- website/src/index.ts
+++ website/src/index.ts
@@ -16,12 +16,15 @@ const validator = zod.object({
 View(() => Vertical(
     Wizard({
         submitAction: async ([ { data: { data } } ]) => {
-            await fetch("/submit", {
+            const req = await fetch("/submit", {
                 method: "POST",
                 body: JSON.stringify(data)
             });
 
-            alert("Abgesendet!")
+            if (req.status != 200)
+                alert(await req.text());
+            else
+                alert("Abgesendet!")
         },
         buttonAlignment: "top",
         buttonArrangement: "flex-end"
@@ -36,10 +39,10 @@ View(() => Vertical(
         }, (data) => [
             Grid(
                 TextInput("text", "Name").sync(data, "name").setWidth("20rem"),
-                DropDownInput("Vortrag 1 (14:00 - 14:20)", presentations[ 0 ].presentations.map(x => `${x.id[ 3 ] ? `${x.id[ 3 ]}:` : ""} ${topics.find(z => z.id == x.id.substring(0, 3))!.title}`)).sync(data, "p1"),
-                DropDownInput("Vortrag 2 (14:30 - 15:00)", presentations[ 1 ].presentations.map(x => `${x.id[ 3 ] ? `${x.id[ 3 ]}:` : ""} ${topics.find(z => z.id == x.id.substring(0, 3))!.title}`)).sync(data, "p2"),
-                DropDownInput("Vortrag 3 (15:10 - 15:20)", presentations[ 2 ].presentations.map(x => `${x.id[ 3 ] ? `${x.id[ 3 ]}:` : ""} ${topics.find(z => z.id == x.id.substring(0, 3))!.title}`)).sync(data, "p3"),
-                DropDownInput("Vortrag 4 (15:30 - 15:50)", presentations[ 3 ].presentations.map(x => `${x.id[ 3 ] ? `${x.id[ 3 ]}:` : ""} ${topics.find(z => z.id == x.id.substring(0, 3))!.title}`)).sync(data, "p4"),
+                DropDownInput("Vortrag 1 (14:00 - 14:20)", presentations[ 0 ].presentations.map(x => x.id)).setRender((key) => `${key[ 3 ] ? `${key[ 3 ]}:` : ""} ${topics.find(z => z.id == key.substring(0, 3))!.title}`).sync(data, "p1"),
+                DropDownInput("Vortrag 2 (14:30 - 15:00)", presentations[ 1 ].presentations.map(x => x.id)).setRender((key) => `${key[ 3 ] ? `${key[ 3 ]}:` : ""} ${topics.find(z => z.id == key.substring(0, 3))!.title}`).sync(data, "p2"),
+                DropDownInput("Vortrag 3 (15:10 - 15:20)", presentations[ 2 ].presentations.map(x => x.id)).setRender((key) => `${key[ 3 ] ? `${key[ 3 ]}:` : ""} ${topics.find(z => z.id == key.substring(0, 3))!.title}`).sync(data, "p3"),
+                DropDownInput("Vortrag 4 (15:30 - 15:50)", presentations[ 3 ].presentations.map(x => x.id)).setRender((key) => `${key[ 3 ] ? `${key[ 3 ]}:` : ""} ${topics.find(z => z.id == key.substring(0, 3))!.title}`).sync(data, "p4"),
             ).setGap("20px").setAlign("center")
         ])
             .setValidator(() => validator)
